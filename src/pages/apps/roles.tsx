93c071db065b3d911ebf8f163dfb17b6b5e7eb51
jooonwood <45617392+jooonwood@users.noreply.github.com>
2021-05-09 12:40:43 +0800

    Create roles.tsx

---
+++ src/pages/apps/roles.tsx
@@ -0,0 +1,146 @@
+import { useState } from 'react';
+import { useParams } from 'umi';
+import { Button, Badge, Popconfirm } from 'antd';
+import ProTable from '@ant-design/pro-table';
+import ProForm, {
+  ModalForm,
+  ProFormText,
+  ProFormTextArea,
+  ProFormSwitch,
+} from '@ant-design/pro-form';
+import { PlusOutlined } from '@ant-design/icons';
+import { queryRoles, setRole } from '@/services/apps';
+
+export default () => {
+  const { app_id } = useParams();
+  const [formVisible, setFormVisible] = useState(false);
+  const [form] = ProForm.useForm();
+  const [data, setData] = useState([]);
+  const columns = [
+    {
+      title: 'Title',
+      dataIndex: 'title',
+      width: 620,
+    },
+    {
+      title: 'Status',
+      dataIndex: 'status',
+      width: 100,
+      hideInSearch: true,
+      render: (_, item) => (
+        <Badge
+          color={item.enabled ? 'green' : 'red'}
+          text={item.enabled ? 'enabled' : 'disabled'}
+        />
+      ),
+    },
+    {
+      title: 'actions',
+      valueType: 'option',
+      width: 40,
+      render: (text, record) => [
+        <Button
+          key="edit"
+          size="small"
+          onClick={() => {
+            setFormVisible(true);
+            form.setFieldsValue({
+              ...record,
+              auth: JSON.stringify(record.auth, null, 2),
+            });
+          }}
+        >
+          Edit
+        </Button>,
+        <Popconfirm
+          key="delete"
+          title="Are you sure？"
+          onConfirm={() => {
+            setData([...data.filter((v) => v.id != record.id)]);
+          }}
+        >
+          <Button size="small" danger>
+            Delete
+          </Button>
+        </Popconfirm>,
+      ],
+    },
+  ];
+
+  return (
+    <>
+      <ModalForm
+        modalProps={{
+          onCancel: () => setFormVisible(false),
+          okText: <>{form.getFieldValue('id') ? 'Save' : 'Create'}</>,
+          cancelText: <>Cancel</>,
+        }}
+        form={form}
+        visible={formVisible}
+        onFinish={async (values) => {
+          const res = await setRole({
+            ...values,
+            auth: JSON.parse(values.auth),
+            app_id,
+          });
+          if (res.response.status == 200) {
+            if (values.id) {
+              setData(data.map((v) => (v.id === res.data.id ? res.data : v)));
+            } else {
+              setData([res.data, ...data]);
+            }
+            setFormVisible(false);
+          }
+        }}
+      >
+        <ProFormText
+          width="xl"
+          name="title"
+          label="title"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        />
+        <ProFormTextArea
+          width="xl"
+          name="auth"
+          label="auth"
+          style={{ width: '100%' }}
+          fieldProps={{ rows: 6 }}
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        />
+        <ProFormSwitch name="enabled" label="enabled" />
+        <ProFormText hidden name="id" />
+      </ModalForm>
+      <ProTable
+        columns={columns}
+        request={async (params = {}, sort, filter) => {
+          const res = await queryRoles({ app_id, params });
+          setData(res.data);
+          return res;
+        }}
+        dataSource={data}
+        rowKey="id"
+        toolBarRender={() => [
+          <Button
+            key="create"
+            icon={<PlusOutlined />}
+            type="primary"
+            onClick={() => {
+              form.resetFields();
+              setFormVisible(true);
+            }}
+          >
+            Create
+          </Button>,
+        ]}
+      />
+    </>
+  );
+};
